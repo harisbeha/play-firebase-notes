6568be36ee7375029514595acb82ab2e6645041c
Haris Beha <harisbeha@gmail.com>
2017-10-24 18:02:34 -0400

    Create Notes controller and related routes
    All routes marked TODO as Play gracefully handles
    unimplemented routes as long as they are explicitly
    defined

---
+++ app/controllers/Notes.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.mvc._
+
+class Notes extends Controller {
+
+  def index = TODO
+
+  def create = TODO
+
+  def read(id: String) = TODO
+
+  def update(id: String) = TODO
+
+  def delete(id: String) = TODO
+
+}
